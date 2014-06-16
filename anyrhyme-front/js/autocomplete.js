e0acd21586733de82f788a1affe795755a61b4af
Robert Kerr <robkerr87@gmail.com>
2014-06-17 08:01:49 +1000

    Added autocomplete to segments.

--- anyrhyme-front/js/autocomplete.js
+++ anyrhyme-front/js/autocomplete.js
@@ -16,7 +16,7 @@ app.directive('autocomplete', function($timeout) {
     },
     controller: function($scope, $element, $attrs){
 
-      $scope.searchParam;
+      $scope.searchParams;
 
       // with the searchFilter the suggestions get filtered
       $scope.searchFilter;
@@ -45,16 +45,16 @@ app.directive('autocomplete', function($timeout) {
       $scope.completing = false;
 
       // starts autocompleting on typing in something
-      $scope.$watch('searchParam', function(){
-        if(watching && $scope.searchParam) {
+      $scope.$watch('searchParams', function(){
+        if(watching && $scope.searchParams) {
           $scope.completing = true;
-          $scope.searchFilter = $scope.searchParam;
+          $scope.searchFilter = $scope.searchParams;
           $scope.selectedIndex = -1;
         }
 
         // function thats passed to on-type attribute gets executed
         if ($scope.onType)
-          $scope.onType($scope.searchParam);
+          $scope.onType($scope.searchParams);
       });
 
       // for hovering over suggestions
@@ -64,7 +64,7 @@ app.directive('autocomplete', function($timeout) {
 
         // this line determines if it is shown 
         // in the input field before it's selected:
-        //$scope.searchParam = suggestion;
+        //$scope.searchParams = suggestion;
 
         $scope.$apply();
         watching = true;
@@ -83,7 +83,7 @@ app.directive('autocomplete', function($timeout) {
       $scope.select = function(suggestion) {
         $scope.time_to_blur = true;
         if(suggestion){
-          $scope.searchParam = suggestion.spelling;
+          $scope.searchParams = suggestion.spelling;
           $scope.searchFilter = suggestion.spelling;
           $scope.onSelect(suggestion);
         }
@@ -96,7 +96,7 @@ app.directive('autocomplete', function($timeout) {
       // submitting text with RIGHT ARROW or ENTER
       $scope.submit = function() {
         $scope.time_to_blur = true;
-        $scope.onSubmit($scope.searchParam);
+        $scope.onSubmit($scope.searchParams);
         watching = false;
         $scope.completing = false;
         setTimeout(function(){watching = true;},1000);
@@ -104,7 +104,7 @@ app.directive('autocomplete', function($timeout) {
       };
 
       $scope.clear_input = function() {
-        $scope.searchParam = "";
+        $scope.searchParams = "";
         $scope.searchFilter = "";
         $scope.suggestions = [];
         $scope.input_cleared = true;
@@ -126,8 +126,8 @@ app.directive('autocomplete', function($timeout) {
         $scope.time_to_blur = false;
       };
 
-      $scope.searchParam = $scope.initial;
-      $scope.onType($scope.searchParam);
+      $scope.searchParams = $scope.initial;
+      $scope.onType($scope.searchParams);
       $scope.submit();
       $scope.input_cleared = false;
       $scope.time_to_focus = false;
@@ -141,7 +141,7 @@ app.directive('autocomplete', function($timeout) {
         scope.placeholder = "start typing..."
       if(attrs["clickActivation"]=="true"){
         element[0].onclick = function(e){
-          if(!scope.searchParam){
+          if(!scope.searchParams){
             scope.completing = true;
             scope.$apply();
           }
@@ -243,7 +243,7 @@ app.directive('autocomplete', function($timeout) {
     },
     template: '<div class="autocomplete">'+
                 '<div class="clear-button" ng-show="input_focus" ng-mousedown="clear_input()" ng-click="return_focus()">✕</div>'+
-                '<input type="text" autocapitalize="off" autocorrect="off" ng-model="searchParam" placeholder="{{placeholder}}" ng-focus="input_focused()" ng-blur="input_blurred()" focus-if="time_to_focus" blur-if="time_to_blur"/>' +
+                '<input type="text" autocapitalize="off" autocorrect="off" ng-model="searchParams" placeholder="{{placeholder}}" ng-focus="input_focused()" ng-blur="input_blurred()" focus-if="time_to_focus" blur-if="time_to_blur"/>' +
                 '<ul ng-show="completing">' +
                   '<li class="pronunciation" suggestion ng-repeat="suggestion in suggestions | filter:{spelling:searchFilter} | orderBy:\'toString()\'" '+
                   'index="{{$index}}" ng-class="{active: '+
@@ -252,7 +252,227 @@ app.directive('autocomplete', function($timeout) {
                   '</li>'+
                 '</ul>'+
               '</div>'
-    // templateUrl: 'script/ac_template.html'
+  }
+});
+
+app.directive('autocompletesegment', function($timeout) {
+  var index = -1;
+
+  return {
+    restrict: 'E',
+    scope: {
+      suggestions: '=data',
+      onType: '=onType',
+      onBlur: '=onBlur',
+      model: '=model'
+    },
+    controller: function($scope, $element, $attrs){
+
+      $scope.model;
+
+      // with the searchFilter the suggestions get filtered
+      $scope.searchFilter;
+
+      // the index of the suggestions that's currently selected
+      $scope.selectedIndex = -1;
+
+      // set new index
+      $scope.setIndex = function(i){
+        $scope.selectedIndex = parseInt(i);
+      }
+
+      this.setIndex = function(i){
+        $scope.setIndex(i);
+        $scope.$apply();
+      }
+
+      $scope.getIndex = function(i){
+        return $scope.selectedIndex;
+      }
+
+      // watches if the parameter filter should be changed
+      var watching = true;
+
+      // autocompleting drop down on/off
+      $scope.completing = false;
+
+      // starts autocompleting on typing in something
+      $scope.$watch('model.label', function(){
+        if(watching && $scope.model.label) {
+          $scope.completing = true;
+          $scope.searchFilter = $scope.model.label;
+          $scope.selectedIndex = -1;
+        }
+
+        // function thats passed to on-type attribute gets executed
+        if ($scope.onType) {
+          $scope.onType($scope.model.label);
+        }
+      });
+
+      // for hovering over suggestions
+      this.preSelect = function(suggestion){
+
+        watching = false;
+
+        // this line determines if it is shown 
+        // in the input field before it's selected:
+        //$scope.model = suggestion;
+
+        $scope.$apply();
+        watching = true;
+
+      }
+
+      $scope.preSelect = this.preSelect;
+
+      this.preSelectOff = function(){
+        watching = true;
+      }
+
+      $scope.preSelectOff = this.preSelectOff;
+
+      // selecting a suggestion with RIGHT ARROW or ENTER
+      $scope.select = function(suggestion) {
+        if(suggestion){
+          $scope.model.label = suggestion.label;
+          $scope.searchFilter = suggestion.label;
+        }
+        watching = false;
+        $scope.completing = false;
+        setTimeout(function(){watching = true;},1000);
+        $scope.setIndex(-1);
+      }
+
+      // submitting text with RIGHT ARROW or ENTER
+      $scope.submit = function() {
+        watching = false;
+        $scope.completing = false;
+        setTimeout(function(){watching = true;},1000);
+        $scope.setIndex(-1);
+      };
+
+      $scope.blur = function() {
+        $scope.onBlur();
+      };
+
+      $scope.onType($scope.model.label);
+      $scope.submit();
+    },
+    link: function(scope, element, attrs){
+
+      if(attrs["clickActivation"]=="true"){
+        element[0].onclick = function(e){
+          if(!scope.model){
+            scope.completing = true;
+            scope.$apply();
+          }
+        };
+      }
+
+      var key = {left: 37, up: 38, right: 39, down: 40 , enter: 13, esc: 27, tab: 9};
+
+      document.addEventListener("keydown", function(e){
+        var keycode = e.keyCode || e.which;
+
+        switch (keycode){
+          case key.esc:
+            // disable suggestions on escape
+            scope.select();
+            scope.setIndex(-1);
+            scope.$apply();
+            e.preventDefault();
+        }
+      }, true);
+
+      element[0].onkeydown = function(e) {
+        var keycode = e.keyCode || e.which;
+
+        var l = angular.element(this).find('li').length;
+
+        // implementation of the up and down movement in the list of suggestions
+        switch (keycode){
+          case key.up:    
+ 
+            index = scope.getIndex()-1;
+            if(index<-1){
+              index = l-1;
+            } else if (index >= l ){
+              index = -1;
+              scope.setIndex(index);
+              scope.preSelectOff();
+              break;
+            }
+            scope.setIndex(index);
+
+            if(index!==-1) {
+              scope.preSelect(scope.suggestions[index]);
+            }
+              
+            scope.$apply();
+
+            break;
+          case key.down:
+            index = scope.getIndex()+1;
+            if(index<-1){
+              index = l-1;
+            } else if (index >= l ){
+              index = -1;
+              scope.setIndex(index);
+              scope.preSelectOff();
+              scope.$apply();
+              break;
+            }
+            scope.setIndex(index);
+            
+            if (index!==-1) {
+              scope.preSelect(scope.suggestions[index]);
+            } 
+
+            break;
+          case key.left:    
+            break;
+          case key.right: 
+          case key.tab:  
+          case key.enter:  
+
+            index = scope.getIndex();
+            // scope.preSelectOff();
+            if (index!==-1) {
+              scope.select(scope.suggestions[index]);
+            } else {
+              scope.submit();
+            }
+            
+            scope.setIndex(-1);  
+            scope.$apply();
+
+            break;
+          case key.esc:
+            // disable suggestions on escape
+            scope.select();
+            scope.setIndex(-1);
+            scope.$apply();
+            e.preventDefault();
+            break;
+          default:
+            return;
+        }
+
+        if(scope.getIndex()!==-1 || keycode == key.enter)
+          e.preventDefault();
+      };
+    },
+    template: '<div class="autocomplete segment">'+
+                '<input type="text" autocapitalize="off" autocorrect="off" ng-model="model.label" ng-blur="blur()"/>' +
+                '<ul ng-show="completing">' +
+                  '<li suggestionsegment ng-repeat="suggestion in suggestions | filter:{label:searchFilter} | orderBy:\'toString()\'" '+
+                  'index="{{$index}}" ng-class="{active: '+
+                  '($index == selectedIndex)}" ng-click="select(suggestion)">'+
+                    '{{suggestion.label}}'+
+                  '</li>'+
+                '</ul>'+
+              '</div>'
   }
 });
 
@@ -260,6 +480,23 @@ app.directive('suggestion', function(){
   return {
     restrict: 'A',
     require: '^autocomplete', // ^look for controller on parents element
+    link: function(scope, element, attrs, autoCtrl){
+      element.bind('mouseenter', function() {
+        autoCtrl.preSelect(attrs['val']);
+        autoCtrl.setIndex(attrs['index']);
+      });
+
+      element.bind('mouseleave', function() {
+        autoCtrl.preSelectOff();
+      });
+    }
+  }
+});
+
+app.directive('suggestionsegment', function(){
+  return {
+    restrict: 'A',
+    require: '^autocompletesegment', // ^look for controller on parents element
     link: function(scope, element, attrs, autoCtrl){
       element.bind('mouseenter', function() {
         autoCtrl.preSelect(attrs['val']);
