9c8f5cda79f1916beb9ca14927a6f6327726d061
Robert Kerr <robrkerr@gmail.com>
2018-01-13 15:39:58 +1100

    Still trying to deploy.

--- anyrhyme-api/config/deploy.rb
+++ anyrhyme-api/config/deploy.rb
@@ -2,6 +2,7 @@
 server '54.158.85.199', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:robrkerr/anyrhyme.git'
+set :repo_tree,       'anyrhyme-api'
 set :application,     'anyrhyme'
 set :user,            'ec2-user'
 set :puma_threads,    [4, 16]
@@ -18,7 +19,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, auth_methods: ["publickey"], keys: ["~/aws-robkerr87-rob.pem"], user: fetch(:user) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
